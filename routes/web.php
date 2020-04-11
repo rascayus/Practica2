6e4a0459f0961cb61f936496308d0a819e000e77
Rascayus <juandialonso@gmail.com>
2020-04-11 11:46:20 +0200

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sobrenosotros', function () {
+    return 'Esta es la página que habla sobre nosotros';
+});
+Route::get('/contacto', function () {
+    return 'Aqui podrás contactar';
+});
+Route::get('/foro', function () {
+    return 'Aqui está el foro';
+});
+Route::get('/post/{id}/{nombre}', function ($id,$nombre) {
+    return 'ESte es el post nº '.$id.' creado por '.$nombre;
+})->where('nombre','[a-z]+');
+
+
