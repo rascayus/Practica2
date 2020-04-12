8a390f61f0ed05cb4310da64a50e403909f581c4
Rascayus <juandialonso@gmail.com>
2020-04-12 13:23:54 +0200

    BBDD Raw

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,20 @@ Route::get('/articulos','MiControlador@store');
 Route::get('/mostrar','MiControlador@show');
 Route::get('/contacto','MiControlador@contactar');
 Route::get('/galeria','MiControlador@galeria');
+Route::get('/insertar',function(){
+    DB::insert("INSERT INTO articulos (NOMBRE_ARTICULO, PRECIO, PAIS_ORIGEN,SECCION,OBSERVACIONES) VALUES (?,?,?,?,?)",
+["JARRON",15.2,"JAPON","CERAMICA","GANGA"]);
+});
+Route::get('/leer',function(){
+    $resultados = DB::select("SELECT * from articulos where ID=?",[2]);
+    foreach ($resultados as $resultado)
+    {
+        return ($resultado->Nombre_articulo);
+    }
+});
+Route::get('/actualiza',function(){
+    DB::update("UPDATE articulos SET SECCION='DECORACION' WHERE ID=?",[2]);
+});
+Route::get('/borra',function(){
+    DB::delete("DELETE from articulos WHERE ID=?",[2]);
+});
